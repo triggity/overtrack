103ec60b74185e46cd520f990d67011cd137d960
Michael Truong <michael@cloudflare.com>
2017-10-31 20:09:51 -0700

    add prometheus

--- server.go
+++ server.go
@@ -1,13 +1,28 @@
 package overtrack
 
 import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/triggity/overtrack/handlers"
 )
 
 func Server(router *mux.Router) {
 
-	router.HandleFunc("/", handlers.Home)
-	router.HandleFunc("/version", handlers.Version)
+	routes := []struct {
+		Route   string
+		Handler http.HandlerFunc
+		Name    string
+	}{
+		{"/", handlers.Home, "home"},
+		{"/version", handlers.Version, "version"},
+	}
+	for _, r := range routes {
+		router.HandleFunc(r.Route, prometheus.InstrumentHandlerFunc(r.Name, r.Handler))
+	}
+	router.Handle("/metrics", promhttp.Handler())
 }
