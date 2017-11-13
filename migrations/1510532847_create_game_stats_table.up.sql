25948a678de7c8554835598719d26ec9bd3d4c5f
Michael Truong <michael@cloudflare.com>
2017-11-12 17:46:24 -0800

    user id

--- migrations/1510532847_create_game_stats_table.up.sql
+++ migrations/1510532847_create_game_stats_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE game_stats (
     id SERIAL,
     game_id integer NOT NULL,
+    user_id integer NOT NULL,
     eliminations integer,
     objective_kills integer,
     objective_time integer,
--- models/stats.go
+++ models/stats.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/jmoiron/sqlx"
+
 type Stats struct {
+	GameID         int `json:"game_id" db:"game_id"`
 	Eliminations   int `json:"eliminations" db:"eliminations"`
 	ObjectiveKills int `json:"objective_kills,omitempty" db:"objective_kills"`
 	ObjectiveTime  int `json:"objective_time" db:"objective_time"`
@@ -11,4 +14,24 @@ type Stats struct {
 
 func (s *Stats) CoreStats() *Stats {
 	return s
+}
+
+type StatsDao struct {
+	db *sqlx.DB
+}
+
+func NewStatsDao(sqlClient *sqlx.DB) *StatsDao {
+	return &StatsDao{sqlClient}
+}
+
+func (s *StatsDao) GetByGame(userId int, gameId int) (Stats, error) {
+	stats := Stats{}
+	err := s.db.Get(&stats, "SELECT * FROM game_stats WHERE user_id=$1 AND game_id=$2 LIMIT 1", userId, gameId)
+	return stats, err
+}
+
+func (s *StatsDao) GetByUser(userId int) ([]Stats, error) {
+	stats := []Stats{}
+	err := s.db.Select(&stats, "SELECT * FROM game_stats WHERE user_id=$1", userId)
+	return stats, err
 }
