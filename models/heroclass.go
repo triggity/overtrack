e79518e24c91772c8c95235ef95138bebb392cc8
Michael Truong <michael@cloudflare.com>
2017-11-12 16:24:09 -0800

    scan all enums

--- models/heroclass.go
+++ models/heroclass.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type HeroClass uint8
 
 const (
@@ -8,3 +15,23 @@ const (
 	Tank
 	Suppport
 )
+
+func (h *HeroClass) Scan(value interface{}) error {
+	// aux := GameType()
+	var aux HeroClass
+	v, ok := value.([]byte)
+	if !ok {
+		return errors.New("value is not of type []byte")
+	}
+	s := string(v)
+	aux, ok = _HeroClassNameToValue[strings.Title(s)]
+	if !ok {
+		return fmt.Errorf("invalid HeroClass %q", s)
+	}
+	*h = aux
+	return nil
+}
+
+func (h HeroClass) Value() (driver.Value, error) {
+	return strings.ToLower(h.String()), nil
+}
--- models/result.go
+++ models/result.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Result uint8
 
 const (
@@ -7,3 +14,23 @@ const (
 	Loss
 	Draw
 )
+
+func (r *Result) Scan(value interface{}) error {
+	// aux := GameType()
+	var aux Result
+	v, ok := value.([]byte)
+	if !ok {
+		return errors.New("value is not of type []byte")
+	}
+	s := string(v)
+	aux, ok = _ResultNameToValue[strings.Title(s)]
+	if !ok {
+		return fmt.Errorf("invalid Result %q", s)
+	}
+	*r = aux
+	return nil
+}
+
+func (r Result) Value() (driver.Value, error) {
+	return strings.ToLower(r.String()), nil
+}
